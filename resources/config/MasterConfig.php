456e2d1557be0ddfbd2255da5bfc9d8aa2fcca84
samu_eyes <samu_eyes@yahoo.com>
2024-06-03 07:55:04 +0700

    update config master

--- resources/config/MasterConfig.php
+++ resources/config/MasterConfig.php
@@ -13,49 +13,53 @@ return [
         'URL' => env('SSO_URL', 'http://sso.test'), /*harus diakhiri dengan / (slash) url untuk login SSO*/
         'CALL_BACK' => '',
     ),
-    'MASTER_TABEL' => ['location', 'project', 'employee', 'item_code', 'uom'],
-    // 'master' => array(
-    //     'employee' => [
-    //         'MODEL' => env('TABEL_MASTER_EMPLOYEE', 'MasterEmployee'),
-    //         'FIELD' => json_decode(env('SYNC_MASTER_EMPLOYEE', '["id","employee_name","employee_job_title","employee_email"]')),
-    //     ],
-    //     'project' => [
-    //         'MODEL' => env('TABEL_MASTER_PROJECT', 'MasterProject'),
-    //         'FIELD' => json_decode(env('SYNC_MASTER_PROJECT', '["id","project_code","project_name"]')),
-    //     ],
-    //     'location' => [
-    //         'MODEL' => env('TABEL_MASTER_LOCATION', 'MasterLocation'),
-    //         'FIELD' => json_decode(env('SYNC_MASTER_LOCATION', '["loc_code","loc_name"]')),
-    //     ],
-    //     'item_code' => [
-    //         'MODEL' => env('TABEL_MASTER_ITEM_CODE', 'MasterItemCode'),
-    //         'FIELD' => json_decode(env('SYNC_MASTER_ITEM_CODE', '["item_code","item_name"]')),
-    //     ],
-    //     'uom' => [
-    //         'MODEL' => env('TABEL_MASTER_UOM', 'MasterUom'),
-    //         'FIELD' => json_decode(env('SYNC_MASTER_UOM', '["id","uom_code","uom_name"]')),
-    //     ],
-    // ),
-    // 'lokal' => array(
-    //     'employee' => [
-    //         'MODEL' => env('TABEL_LOKAL_EMPLOYEE', 'Employee'),
-    //         'FIELD' => json_decode(env('SYNC_LOKAL_EMPLOYEE', '["id","name","job_title","email"]')),
-    //     ],
-    //     'project' => [
-    //         'MODEL' => env('TABEL_LOKAL_PROJECT', 'Project'),
-    //         'FIELD' => json_decode(env('SYNC_LOKAL_PROJECT', '["id","project_code","project_name"]')),
-    //     ],
-    //     'location' => [
-    //         'MODEL' => env('TABEL_LOKAL_LOCATION', 'Location'),
-    //         'FIELD' => json_decode(env('SYNC_LOKAL_LOCATION', '["location_code","location_name"]')),
-    //     ],
-    //     'item_code' => [
-    //         'MODEL' => env('TABEL_LOKAL_ITEM_CODE', 'Item'),
-    //         'FIELD' => json_decode(env('SYNC_LOKAL_ITEM_CODE', '["item_code","item_desc"]')),
-    //     ],
-    //     'uom' => [
-    //         'MODEL' => env('TABEL_LOKAL_UOM', 'Uom'),
-    //         'FIELD' => json_decode(env('SYNC_LOKAL_UOM', '["id","uom_code","uom_name"]')),
-    //     ],
-    // )
+    'MASTER_TABEL' => ['location', 'project', 'project_detail', 'employee', 'item_code', 'uom'],
+    'master' => array(
+        'employee' => [
+            'MODEL' => env('TABEL_MASTER_EMPLOYEE', 'MasterEmployee'),
+            'FIELD' => json_decode(env('SYNC_MASTER_EMPLOYEE', '["id","employee_name","employee_job_title","employee_email"]')),
+        ],
+        'project' => [
+            'MODEL' => env('TABEL_MASTER_PROJECT', 'MasterProject'),
+            'FIELD' => json_decode(env('SYNC_MASTER_PROJECT', '["id","project_code","project_name"]')),
+        ],
+        'location' => [
+            'MODEL' => env('TABEL_MASTER_LOCATION', 'MasterLocation'),
+            'FIELD' => json_decode(env('SYNC_MASTER_LOCATION', '["loc_code","loc_name"]')),
+        ],
+        'item_code' => [
+            'MODEL' => env('TABEL_MASTER_ITEM_CODE', 'MasterItemCode'),
+            'FIELD' => json_decode(env('SYNC_MASTER_ITEM_CODE', '["item_code","item_name"]')),
+        ],
+        'uom' => [
+            'MODEL' => env('TABEL_MASTER_UOM', 'MasterUom'),
+            'FIELD' => json_decode(env('SYNC_MASTER_UOM', '["id","uom_code","uom_name"]')),
+        ],
+    ),
+    'lokal' => array(
+        'employee' => [
+            'MODEL' => env('TABEL_LOKAL_EMPLOYEE', 'Employee'),
+            'FIELD' => json_decode(env('SYNC_LOKAL_EMPLOYEE', '["id","name","job_title","email"]')),
+        ],
+        'project' => [
+            'MODEL' => env('TABEL_LOKAL_PROJECT', 'Project'),
+            'FIELD' => json_decode(env('SYNC_LOKAL_PROJECT', '["id","project_code","project_name"]')),
+        ],
+        'project_detail' => [
+            'MODEL' => env('TABEL_LOKAL_PROJECT_DETAIL', 'ProjectDetail'),
+            // 'FIELD' => json_decode(env('SYNC_LOKAL_PROJECT', '["id","project_code","project_name"]')), comment untuk sync semua filed
+        ],
+        'location' => [
+            'MODEL' => env('TABEL_LOKAL_LOCATION', 'Location'),
+            'FIELD' => json_decode(env('SYNC_LOKAL_LOCATION', '["location_code","location_name"]')),
+        ],
+        'item_code' => [
+            'MODEL' => env('TABEL_LOKAL_ITEM_CODE', 'Item'),
+            'FIELD' => json_decode(env('SYNC_LOKAL_ITEM_CODE', '["item_code","item_desc"]')),
+        ],
+        'uom' => [
+            'MODEL' => env('TABEL_LOKAL_UOM', 'Uom'),
+            'FIELD' => json_decode(env('SYNC_LOKAL_UOM', '["id","uom_code","uom_name"]')),
+        ],
+    )
 ];
