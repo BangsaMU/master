120a92106c6c42f9af3da7e7ae127f1743f58966
IT <hatur.cms@gmail.com>
2024-10-23 09:43:52 +0700

    update cek action admin

--- src/Controllers/ProjectController.php
+++ src/Controllers/ProjectController.php
@@ -297,7 +297,7 @@ class ProjectController extends Controller
                 }
                 $nestedData['No'] = $DT_RowIndex;
 
-                if (checkPermission('is_admin')) {
+                if (config('MasterCrudConfig.MASTER_DIRECT_EDIT') == true && checkPermission('is_admin')) {
                     $btn .= '<a href="' . route('master.' . $sheet_slug . '.edit', $row->No) . '" class="btn btn-primary btn-sm">Update</a> ';
                     $btn .= '<a href="' . route('master.' . $sheet_slug . '.destroy', $row->No) . '" onclick="notificationBeforeDelete(event,this)" class="btn btn-danger btn-sm">Delete</a>';
                 } else {
