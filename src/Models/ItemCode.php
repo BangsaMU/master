d0ae7035dcf20686cfc495caaf58dd78a573b370
IT <hatur.cms@gmail.com>
2025-04-10 14:33:25 +0700

    add itemcode attributes

--- src/Models/ItemCode.php
+++ src/Models/ItemCode.php
@@ -34,6 +34,7 @@ class ItemCode extends Model
                 $table->dateTime('updated_at')->nullable();
                 $table->dateTime('deleted_at')->nullable();
                 $table->string('app_code', 10)->default('APP03');
+                $table->longText('attributes')->nullable();
 
                 $table->index('app_code', 'index_app_code');
             });
