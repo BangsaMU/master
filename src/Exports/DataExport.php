22eb846987f978cf96262f033b42988cb9c8de1b
GITA <gita.samudra@meindo.com>
2026-07-02 13:10:54 +0700

    export by filter untuk item code

--- src/Exports/DataExport.php
+++ src/Exports/DataExport.php
@@ -6,6 +6,7 @@ use Bangsamu\LibraryClay\Controllers\LibraryClayController;
 use Bangsamu\Master\Models\DashboardSettings;
 use Bangsamu\Master\Models\Setting;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -41,12 +42,14 @@ class DataExport implements FromCollection, ShouldAutoSize, WithHeadings
             ->select($headers)
             ->whereNull('deleted_at');
 
-        if ($this->table === 'master_location') {
-            $groupTypes = $this->getAllowedGroupTypes();
+        $filters = $this->getDynamicFilters($databaseConfig);
 
-            if (!empty($groupTypes)) {
-                $query->whereIn('group_type', $groupTypes);
-            }
+        if (!empty($filters)) {
+            $query->where(function ($q) use ($filters) {
+                foreach ($filters as $filter) {
+                    $q->orWhereIn($filter['column'], $filter['values']);
+                }
+            });
         }
 
         return $query->get();
@@ -71,20 +74,28 @@ class DataExport implements FromCollection, ShouldAutoSize, WithHeadings
         return $headers;
     }
 
-    protected function getAllowedGroupTypes(): array
+    protected function getDynamicFilters(string $databaseConfig): array
     {
-        $listLocation = Setting::where('name', 'group_type')
-            ->where('category', 'master_location')
-            ->value('value');
+        $settings = Setting::where('category', $this->table)->get();
 
-        if (empty($listLocation)) {
-            $listLocation = DashboardSettings::where('key', 'group_type')
-                ->where('group', 'master_location')
-                ->value('value');
+        if ($settings->isEmpty()) {
+            $settings = DashboardSettings::where('group', $this->table)->get();
         }
 
-        $listLocation = array_filter(array_map('trim', explode(',', (string) $listLocation)));
+        $filters = [];
 
-        return !empty($listLocation) ? array_values($listLocation) : [];
+        foreach ($settings as $setting) {
+            $column = $setting->name ?? $setting->key;
+            $values = array_values(array_filter(array_map('trim', explode(',', (string) ($setting->value ?? '')))));
+
+            if (!empty($column) && !empty($values) && Schema::connection($databaseConfig)->hasColumn($this->table, $column)) {
+                $filters[] = [
+                    'column' => $column,
+                    'values' => $values,
+                ];
+            }
+        }
+
+        return $filters;
     }
 }
--- src/Exports/Master/ItemCodeExport.php
+++ src/Exports/Master/ItemCodeExport.php
@@ -2,7 +2,10 @@
 
 namespace Bangsamu\Master\Exports\Master;
 
+use Bangsamu\Master\Models\DashboardSettings;
+use Bangsamu\Master\Models\Setting;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -12,23 +15,41 @@ class ItemCodeExport implements FromCollection, ShouldAutoSize, WithHeadings
 
     public function collection()
     {
-        $data = DB::table('master_item_code as mic')
-                    ->leftJoin('master_uom as mu', 'mu.id', 'mic.uom_id')
-                    ->leftJoin('master_pca as mp', 'mp.id', 'mic.pca_id')
-                    ->leftJoin('master_category as mc', 'mc.id', 'mic.category_id')
-                    ->leftJoin('master_item_group as mig', 'mig.id', 'mic.group_id')
-                    ->select(
-                        'mic.item_code',
-                        'mic.item_name',
-                        'mu.uom_name',
-                        'mp.pca_name',
-                        'mc.category_name',
-                        'mig.item_group_name',
-                    )
-                    ->whereNull('mic.deleted_at')
-                    ->get();
+        $query = DB::table('master_item_code as mic')
+            ->leftJoin('master_uom as mu', 'mu.id', 'mic.uom_id')
+            ->leftJoin('master_pca as mp', 'mp.id', 'mic.pca_id')
+            ->leftJoin('master_category as mc', 'mc.id', 'mic.category_id')
+            ->leftJoin('master_item_group as mig', 'mig.id', 'mic.group_id')
+            ->select(
+                'mic.item_code',
+                'mic.item_name',
+                'mu.uom_name',
+                'mp.pca_name',
+                'mc.category_name',
+                'mig.item_group_name',
+            )
+            ->whereNull('mic.deleted_at');
 
-        return $data;
+        $settings = Setting::where('category', 'master_item_code')->get();
+
+        if ($settings->isEmpty()) {
+            $settings = DashboardSettings::where('group', 'master_item_code')->get();
+        }
+
+        if (!$settings->isEmpty()) {
+            $query->where(function ($q) use ($settings) {
+                foreach ($settings as $setting) {
+                    $column = $setting->name ?? $setting->key;
+                    $values = array_values(array_filter(array_map('trim', explode(',', (string) ($setting->value ?? '')))));
+
+                    if (!empty($column) && !empty($values) && Schema::hasColumn('master_item_code', $column)) {
+                        $q->orWhereIn("mic.$column", $values);
+                    }
+                }
+            });
+        }
+
+        return $query->get();
     }
 
     public function headings(): array
